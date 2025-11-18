85c80c30f9a3b803a1ae2e28d46287cf61dec33c
EnUfor <50682605+EnUfor@users.noreply.github.com>
2025-11-18 13:48:49 -0500

    Add handling when no IIS websites present

--- plugins/modules/website_info.ps1
+++ plugins/modules/website_info.ps1
@@ -74,8 +74,12 @@ try {
     }
     # Return information of all the websites available on the system
     else {
-        $WebsiteList = $(Get-Website).Name
-        $module.Result.site = @($WebsiteList | ForEach-Object { Get-WebsiteInfo -name $_ })
+        $WebsiteList = @(Get-Website)
+        if ($WebsiteList.Count -gt 0) {
+            $module.Result.site = @($WebsiteList | ForEach-Object { Get-WebsiteInfo -name $_.Name })
+        } else {
+            $module.Result.site = @()
+        }
     }
 }
 catch {
