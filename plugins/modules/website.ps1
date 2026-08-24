1a57928a0ca66dee0377d8730ba0758b3b3201d5
n90-jd <jd@n90.io>
2026-08-24 02:29:22 +0100

    limit discovered bindings to supported protocols (#71)
    * limit discovered bindings to supported protocols
    
    * satisfy lint

--- plugins/modules/website.ps1
+++ plugins/modules/website.ps1
@@ -217,6 +217,7 @@ Try {
         # Add Remove or Set bindings if needed
         if ( $null -ne $bindings.set -or $bindings.add.Count -gt 0 -or $bindings.remove.Count -gt 0 ) {
             $site_bindings = (Get-ItemProperty -LiteralPath "IIS:\Sites\$($site.Name)").Bindings.Collection
+            $site_bindings = $site_bindings.Where({ $_.protocol -in $binding_options.options.protocol.choices })
             $toAdd = @()
             $toEdit = @()
             $toRemove = @()
